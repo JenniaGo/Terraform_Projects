dc22cdf8ee6ac727b95b69f69bb1749dfe486bf7
JenniaGo <jenniagorelik@gmail.com>
2022-05-19 15:04:41 +0300

    Update variables.tf

--- Create_S3_Bucket/variables.tf
+++ Create_S3_Bucket/variables.tf
@@ -1,13 +1,13 @@
 variable "cli_usr_access_key" {
   type    = string
-  default = "usr_access_key" # specify the access key
+  default = "AKIA5QIE5TSXHPG2J3VW" # specify the access key
 }
 variable "cli_usr_secret_key" {
   type    = string
-  default = "usr_secret_key" # specify the secret key
+  default = "lnnAJ6Hoc7ICgcc4ESQ4f7/Sh8Z4XgVAtdgvy+hR" # specify the secret key
 }
 variable "selected_region" {
   type    = string
-  default = "ap-south-1" # specify the aws region
+  default = "us-east-1" # specify the aws region
 
 }
