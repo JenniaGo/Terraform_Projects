02b3bcc948713a3e997c90d56e97d6b48b446857
JenniaGo <jenniagorelik@gmail.com>
2022-05-22 10:53:13 +0300

    Update variables.tf

--- Create_S3_Bucket/variables.tf
+++ Create_S3_Bucket/variables.tf
@@ -1,10 +1,10 @@
 variable "cli_usr_access_key" {
   type    = string
-  default = "AKIA5QIE5TSXHPG2J3VW" # specify the access key
+  default = "AK********" # specify the access key | or /aws/reference/secretsmanager/s1-secret
 }
 variable "cli_usr_secret_key" {
   type    = string
-  default = "lnnAJ6Hoc7ICgcc4ESQ4f7/Sh8Z4XgVAtdgvy+hR" # specify the secret key
+  default = "ln*******************" # specify the secret key | or /aws/reference/secretsmanager/s2-secret
 }
 variable "selected_region" {
   type    = string
